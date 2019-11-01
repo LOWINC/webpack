8786614a21ff80a8804bbb3e6aef0855fa0c7275
sunweiqi <921939025@qq.com>
2019-11-01 18:23:58 +0800

    clean dist

--- webpack/index.common.js
+++ webpack/index.common.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const webpack = require("webpack");
 const devMode = process.env.NODE_ENV !== "production";
 module.exports = {
@@ -17,5 +16,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [new CleanWebpackPlugin(), new webpack.ProgressPlugin()]
+  plugins: [new webpack.ProgressPlugin()]
 };
--- webpack/index.prod.js
+++ webpack/index.prod.js
@@ -4,6 +4,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 const webpack = require("webpack");
 const path = require("path");
 const pkg = require("../package.json");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   entry: {
@@ -19,7 +20,8 @@ module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production")
-    })
+    }),
+    new CleanWebpackPlugin()
   ],
   optimization: {
     minimize: true,
