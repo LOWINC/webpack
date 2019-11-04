41fe52c3c34839ba08ee4dde02602599e00a48d5
sunweiqi <921939025@qq.com>
2019-11-04 11:40:12 +0800

    jest

---
+++ src/__test__/index.test.ts
@@ -0,0 +1,18 @@
+import {Constants, Utils} from "../index";
+
+const {
+  Order: {MapStatusToStr, OrderStatus}
+} = Constants;
+const {
+  Order: {isInitOrder}
+} = Utils;
+
+test("订单状态文案", () => {
+  expect(MapStatusToStr[OrderStatus.Init]).toBe("待支付");
+  expect(MapStatusToStr[OrderStatus.Paying]).toBe("支付中");
+});
+
+test("订单状态判断", () => {
+  expect(isInitOrder("Init")).toBe(true);
+  expect(isInitOrder("Paying")).toBe(false);
+});
